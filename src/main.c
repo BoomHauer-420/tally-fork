815827f8419a50890067e7b51ecca61f1b6bb682
Clancy Boomhauer <nicolai20044@gmail.com>
2026-05-20 23:26:23 +0200

    feat: support multiple file arguments (#7)
    * feat: support multiple file arguments
    
    This feature adds a support for reading and processing mutiple files at once.
    Also outputs a total of all count operations.
    
    Closes #3

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,74 +11,107 @@ int main(int argc, char *argv[]) {
         "<filename> = count words\nUsage: tally -c <filename> "
         "= count characters\n";
 
-    if (argc < 2 || argc > 3) {
+    if (argc < 2) {
         fprintf(stderr, "%s", Usage);
         return EXIT_FAILURE;
     }
-    char *filename;
-    if (argc == 3) {
+    int start;
+    long total = 0;
+    int error = 0;
+    long max = 0;
 
-        if (strcmp(argv[1], "-w") != 0 && strcmp(argv[1], "-c") != 0) {
-            fprintf(stderr, "%s", Usage);
-            return EXIT_FAILURE;
-        }
-
-        filename = argv[2];
+    if (strcmp(argv[1], "-w") == 0 || strcmp(argv[1], "-c") == 0) {
+        start = 2;
     } else {
-
-        if (argc == 2 && strcmp(argv[1], "-w") == 0) {
-            fprintf(stderr, "%s", Usage);
-            return EXIT_FAILURE;
-        }
-
-        else if (argc == 2 && strcmp(argv[1], "-c") == 0) {
-            fprintf(stderr, "%s", Usage);
-            return EXIT_FAILURE;
-        }
-
-        filename = argv[1];
+        start = 1;
     }
 
-    FILE *fp = fopen(filename, "r");
-    if (!fp) {
-        perror(filename);
+    for (int i = start; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        if (!fp) {
+            error = 1;
+            continue;
+        }
+        if (strcmp(argv[1], "-w") == 0) {
+            long words = tally_count_words(fp);
+            fclose(fp);
+
+            if (words > max) {
+                max = words;
+            }
+            continue;
+        } else if (strcmp(argv[1], "-c") == 0) {
+            long characters = tally_count_characters(fp);
+            fclose(fp);
+
+            if (characters > max) {
+                max = characters;
+            }
+            continue;
+
+        } else {
+            long lines = tally_count_lines(fp);
+            fclose(fp);
+
+            if (lines > max) {
+                max = lines;
+            }
+            continue;
+        }
+    }
+    int width = (max > 0) ? (int)log10((double)max) + 1 : 1;
+
+    for (int i = start; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        if (!fp) {
+            perror(argv[i]);
+            error = 1;
+            continue;
+        }
+        if (strcmp(argv[1], "-w") == 0) {
+            long words = tally_count_words(fp);
+            fclose(fp);
+
+            if (words < 0) {
+                fprintf(stderr, "tally: internal error while counting\n");
+                continue;
+            }
+            total += words;
+
+            printf("%*ld\t%s\n", width, words, argv[i]);
+            continue;
+        } else if (strcmp(argv[1], "-c") == 0) {
+            long characters = tally_count_characters(fp);
+            fclose(fp);
+
+            if (characters < 0) {
+                fprintf(stderr, "tally: internal error while counting\n");
+                continue;
+            }
+            total += characters;
+
+            printf("%*ld\t%s\n", width, characters, argv[i]);
+            continue;
+
+        } else {
+            long lines = tally_count_lines(fp);
+            fclose(fp);
+
+            if (lines < 0) {
+                fprintf(stderr, "tally: internal error while counting\n");
+                continue;
+            }
+            total += lines;
+
+            printf("%*ld\t%s\n", width, lines, argv[i]);
+            continue;
+        }
+    }
+
+    printf("\n%*ld\ttotal\n", width, total);
+
+    if (error) {
         return EXIT_FAILURE;
     }
-    if (strcmp(argv[1], "-w") == 0) {
-        long words = tally_count_words(fp);
-        fclose(fp);
-
-        if (words < 0) {
-            fprintf(stderr, "tally: internal error while counting\n");
-            return EXIT_FAILURE;
-        }
-
-        printf("%ld\n", words);
-        return EXIT_SUCCESS;
-    }
-
-    else if (strcmp(argv[1], "-c") == 0) {
-        long characters = tally_count_characters(fp);
-        fclose(fp);
-
-        if (characters < 0) {
-            fprintf(stderr, "tally: internal error while counting\n");
-            return EXIT_FAILURE;
-        }
-
-        printf("%ld\n", characters);
-        return EXIT_SUCCESS;
-
-    } else {
-        long lines = tally_count_lines(fp);
-        fclose(fp);
-
-        if (lines < 0) {
-            fprintf(stderr, "tally: internal error while counting\n");
-            return EXIT_FAILURE;
-        }
-
-        printf("%ld\n", lines);
-        return EXIT_SUCCESS;
-    }
+    return EXIT_SUCCESS;
 }
